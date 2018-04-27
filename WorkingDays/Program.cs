41e6ed7b9fe1e1add18a36382b34a4a473fc8185
lutfiqaraman <lutfiqaraman@gmail.com>
2018-04-27 20:56:06 +0300

    Done

--- WorkingDays/Program.cs
+++ WorkingDays/Program.cs
@@ -10,6 +10,36 @@ namespace WorkingDays
     {
         static void Main(string[] args)
         {
+            int daysoff = 0;
+
+            //off days are : Friday and Saturday
+            string[] daysofweekend = 
+            {
+                 DayOfWeek.Friday.ToString(),
+                 DayOfWeek.Saturday.ToString()
+            };
+
+            //1st April 2018
+            DateTime startDate = new DateTime(2018, 4, 1);
+
+            //1st May 2018
+            DateTime endDate = new DateTime(2018, 5, 1);
+
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (date.DayOfWeek.ToString() == daysofweekend[0] || date.DayOfWeek.ToString() == daysofweekend[1])
+                    daysoff += 1;                
+            }
+
+            int totalDays = (int) (endDate - startDate).TotalDays;
+            int workingdays = totalDays - daysoff;
+
+            Console.WriteLine("From: " + startDate.ToShortDateString() + " To: " + endDate.ToShortDateString());
+            Console.WriteLine("Days : {0}", totalDays);
+            Console.WriteLine("Working Days: {0}", workingdays);
+            Console.WriteLine("Off Days: {0}", daysoff);
+
+            Console.ReadKey();
         }
     }
 }
